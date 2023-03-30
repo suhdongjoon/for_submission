af6c56a9160827a1af3cc994df34a6cc32f03d2c
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-03-30 10:12:47 +0900

    a

--- project2/project2.py
+++ project2/project2.py
@@ -6,12 +6,12 @@ st.title("🦾입력한 정보로 분석한 결과 입니다🦾")
 st.markdown("---")
 
 st.sidebar.header('Menu')
-age = st.slider('나이', 0, 100)
+age = st.sidebar.slider('나이', 0, 100)
 sex = st.sidebar.selectbox('성별', [True, False])
 hd = st.sidebar.selectbox('심장병', [True, False])
-bp = st.slider('안정혈압', 90, 200)
-col = st.slider('콜레스테롤', 120, 564)
-hb = st.slider('최대심박수', 70, 202)
+bp = st.sidebar.slider('안정혈압', 90, 200)
+col = st.sidebar.slider('콜레스테롤', 120, 564)
+hb = st.sidebar.slider('최대심박수', 70, 202)
 
 
 
