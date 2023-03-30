b1b9fdd092ad3cd05fe2cbc49856ef52f64ca55e
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-03-30 13:58:32 +0900

    67

--- project2/project2.py
+++ project2/project2.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 from joblib import load
 import xgboost
 
-st.markdown('<div><a href="https://sparkly-prince-933.notion.site/1ccb865a95e54590bfd61e22b45520fa"><img src="https://i.imgur.com/ktulthH.gif" width=700></a></div>', unsafe_allow_html=True)
-
 
 st.sidebar.header('Menu')
 age = st.sidebar.slider('나이', 0, 100)
@@ -16,26 +14,29 @@ hd = st.sidebar.selectbox('심장병', ['심장병 있음', '심장병 없음'])
 heart_disease = True if hd == '심장병 있음' else False
 bp = st.sidebar.slider('안정혈압', 90, 200)
 col = st.sidebar.slider('콜레스테롤', 120, 564)
-hb = st.sidebar.slider('최대심박수', 70, 202,0)
+hb = st.sidebar.slider('최대심박수', 70, 202)
 
+if hb != "":
+    jobs = load("project2/xgb_model.joblib")
+    prob = jobs.predict_proba([[age,gender,heart_disease,bp,col,hb]])[:, 1]
+    st.write(prob)
 
-jobs = load("project2/xgb_model.joblib")
-prob = jobs.predict_proba([[age,gender,heart_disease,bp,col,hb]])[:, 1]
-st.write(prob)
+    probabilities = []
 
-probabilities = []
-
-for col_val in range(col, 150, -1):
-    prob = jobs.predict_proba([[age,gender,heart_disease,bp,col_val,hb]])[:, 1]
-    probabilities.append(prob)
+    for col_val in range(col, 150, -1):
+        prob = jobs.predict_proba([[age,gender,heart_disease,bp,col_val,hb]])[:, 1]
+        probabilities.append(prob)
     
-    if prob < 0.5:
-        break
+        if prob < 0.5:
+            break
 
 
-fig, ax = plt.subplots()
-ax.plot(range(col, col-len(probabilities), -1), probabilities)
-ax.set_xlabel("Cholesterol")
-ax.set_ylabel("Probability of Heart Disease")
-ax.set_title("Probability of Heart Disease by Cholesterol Level")
-st.pyplot(fig)
+    fig, ax = plt.subplots()
+    ax.plot(range(col, col-len(probabilities), -1), probabilities)
+    ax.set_xlabel("Cholesterol")
+    ax.set_ylabel("Probability of Heart Disease")
+    ax.set_title("Probability of Heart Disease by Cholesterol Level")
+    st.pyplot(fig)
+
+else:
+    st.markdown('<div><a href="https://sparkly-prince-933.notion.site/1ccb865a95e54590bfd61e22b45520fa"><img src="https://i.imgur.com/ktulthH.gif" width=700></a></div>', unsafe_allow_html=True)
