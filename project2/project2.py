2696e8edba4193c44018a879526dad821026af6a
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-03-30 10:25:02 +0900

    41

--- project2/project2.py
+++ project2/project2.py
@@ -13,11 +13,13 @@ hd = st.sidebar.selectbox('심장병', [True, False])
 bp = st.sidebar.slider('안정혈압', 90, 200)
 col = st.sidebar.slider('콜레스테롤', 120, 564)
 hb = st.sidebar.slider('최대심박수', 70, 202)
-
+"""
 jobs = load("project2/xgb_model.joblib")
 prob = jobs.predict_proba([[age,sex,hd,bp,col,hb]])[:, 1]
 st.write(prob)
-
+"""
+import os
+st.write(os.getcwd())
 
 
 
