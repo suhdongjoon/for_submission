1a3a517e597638e75c776e805063855be078623c
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-03-30 11:51:21 +0900

    123

--- project2/project2.py
+++ project2/project2.py
@@ -18,6 +18,17 @@ bp = st.sidebar.slider('안정혈압', 90, 200)
 col = st.sidebar.slider('콜레스테롤', 120, 564)
 hb = st.sidebar.slider('최대심박수', 70, 202)
 
+
 jobs = load("project2/xgb_model.joblib")
 prob = jobs.predict_proba([[age,gender,heart_disease,bp,col,hb]])[:, 1]
 st.write(prob)
+
+for col_val in range(col, 150, -1):
+    probabilities.append(prob)
+    if prob < 0.5:
+        break
+plt.plot(range(col, col-len(probabilities), -1), probabilities)
+plt.xlabel("Cholesterol")
+plt.ylabel("Probability of Heart Disease")
+plt.title("Probability of Heart Disease by Cholesterol Level")
+plt.show()
