b8ff23a1c8bc3b0fbac61ca845a99eebd02f9839
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-03-30 11:26:46 +0900

    123

--- project2/project2.py
+++ project2/project2.py
@@ -10,13 +10,9 @@ st.markdown("---")
 
 st.sidebar.header('Menu')
 age = st.sidebar.slider('나이', 0, 100)
-sex = st.sidebar.selectbox('성별', ['남자', '여자'])
-hd_str = st.sidebar.selectbox('심장병', ['심장병 있음', '심장병 없음'])
+sex = st.sidebar.checkbox('남자')
+hd = st.sidebar.checkbox('심장병 있음')
 
-if hd_str == '심장병 있음':
-    hd = True
-else:
-    hd = False
 bp = st.sidebar.slider('안정혈압', 90, 200)
 col = st.sidebar.slider('콜레스테롤', 120, 564)
 hb = st.sidebar.slider('최대심박수', 70, 202)
