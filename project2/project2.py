3a53d8e25c7b480099f884e996ebea64b2414b33
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-03-30 16:39:49 +0900

    13

--- project2/project2.py
+++ project2/project2.py
@@ -16,16 +16,15 @@ heart_disease = True if hd == '심장병 있음' else False
 bp = st.sidebar.slider('안정혈압', 90, 200)
 col = st.sidebar.slider('콜레스테롤', 120, 564)
 hb = st.sidebar.slider('최대심박수', 70, 202, None)
-st.markdown('<a href="https://sparkly-prince-933.notion.site/1ccb865a95e54590bfd61e22b45520fa"><img src="https://i.imgur.com/ktulthH.gif" width=800></a>', unsafe_allow_html=True)
+k = st.markdown('<a href="https://sparkly-prince-933.notion.site/1ccb865a95e54590bfd61e22b45520fa"><img src="https://i.imgur.com/ktulthH.gif" width=800></a>', unsafe_allow_html=True)
 btn_clicked1 = st.sidebar.button("Confirm")
 
 if btn_clicked1 == True:
-    st.empty()
     jobs = load("project2/xgb_model.joblib")
     tf = jobs.predict([[age,gender,heart_disease,bp,col,hb]])
     tf_p = jobs.predict_proba([[age,gender,heart_disease,bp,col,hb]])[:, 1]
 
-    st.title("🦾입력한 정보로 분석한 결과 입니다🦾")
+    k = st.title("🦾입력한 정보로 분석한 결과 입니다🦾")
     st.markdown("---")
 
 
