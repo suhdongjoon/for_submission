bf3d95988ea872e3206882fbf6fd8ff9b390332c
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-03-30 16:49:32 +0900

    1

--- project2/project2.py
+++ project2/project2.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 from joblib import load
 import xgboost
 
-
-
 st.sidebar.header('Menu')
 age = st.sidebar.slider('나이', 0, 100)
 sex = st.sidebar.selectbox('성별', ['남자', '여자'])
@@ -18,20 +16,16 @@ col = st.sidebar.slider('콜레스테롤', 120, 564)
 hb = st.sidebar.slider('최대심박수', 70, 202, None)
 gear = st.markdown('<a href="https://sparkly-prince-933.notion.site/1ccb865a95e54590bfd61e22b45520fa"><img src="https://i.imgur.com/ktulthH.gif" width=800></a>', unsafe_allow_html=True)
 btn_clicked1 = st.sidebar.button("Confirm")
+jobs = load("project2/xgb_model.joblib")    
+tf = jobs.predict([[age,gender,heart_disease,bp,col,hb]])
+tf_p = jobs.predict_proba([[age,gender,heart_disease,bp,col,hb]])[:, 1]
 
 if btn_clicked1 == True:
     gear.empty()
-    jobs = load("project2/xgb_model.joblib")
-    tf = jobs.predict([[age,gender,heart_disease,bp,col,hb]])
-    tf_p = jobs.predict_proba([[age,gender,heart_disease,bp,col,hb]])[:, 1]
-
     st.title("🦾입력한 정보로 분석한 결과 입니다🦾")
     st.markdown("---")
-
-
     if tf == 1 : st.write("# 분석 결과 🤦‍♂️ <span style='color:red'>고혈압</span> 🤦‍♂️입니다.", unsafe_allow_html=True)
     if tf == 0 : st.write("# 분석 결과 😊 <span style='color:blue'>정상</span> 😊입니다.", unsafe_allow_html=True)
-        
     st.write(f"""
         ## 👇분석 결과👇
         ### 👉 성별 : {sex}
@@ -48,16 +42,13 @@ if btn_clicked1 == True:
     """)
 btn_clicked2 = st.button("Next")
 
-if btn_clicked2 == True and jobs.predict([[age,gender,heart_disease,bp,col,hb]]) == 1:
-    jobs = load("project2/xgb_model.joblib")    
+if btn_clicked2 == True and tf == 1:
     probabilities = []
     for col_val in range(col, 150, -1):
-        tf_p = jobs.predict_proba([[age,gender,heart_disease,bp,col_val,hb]])[:, 1]
-        probabilities.append(tf_p)
-        if tf_p < 0.5:
+        prob = jobs.predict_proba([[age,gender,heart_disease,bp,col_val,hb]])[:, 1]
+        probabilities.append(prob)
+        if prob < 0.5:
             break
-
-
     fig, ax = plt.subplots()
     ax.plot(range(col, col-len(probabilities), -1), probabilities)
     ax.set_xlabel("Cholesterol")
