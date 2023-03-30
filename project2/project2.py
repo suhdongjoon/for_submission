eb9d8f34f8096febad4fa0234075ff00fd0662fd
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-03-30 17:04:05 +0900

    2

--- project2/project2.py
+++ project2/project2.py
@@ -57,4 +57,4 @@ if btn_clicked2 == True and tf == 1:
     st.pyplot(fig)
     gear.empty()
 if tf == 0:
-    gear.markdown("https://i.imgur.com/4Xd3Mdn.gif", width = 800)
+    gear.image("https://i.imgur.com/4Xd3Mdn.gif", width = 800)
