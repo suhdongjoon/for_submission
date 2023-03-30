4a5e3ca398bd54c90bc0510af04cda3f410e80a2
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-03-30 14:04:19 +0900

    135

--- project2/project2.py
+++ project2/project2.py
@@ -14,9 +14,9 @@ hd = st.sidebar.selectbox('심장병', ['심장병 있음', '심장병 없음'])
 heart_disease = True if hd == '심장병 있음' else False
 bp = st.sidebar.slider('안정혈압', 90, 200)
 col = st.sidebar.slider('콜레스테롤', 120, 564)
-hb = int(st.sidebar.text_input('최대심박수'))
+hb = st.sidebar.slider('최대심박수', 70, 202)
 
-if hb != "":
+if jobs is not None:
     jobs = load("project2/xgb_model.joblib")
     prob = jobs.predict_proba([[age,gender,heart_disease,bp,col,hb]])[:, 1]
     st.write(prob)
