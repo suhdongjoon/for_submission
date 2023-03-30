f18574a0a4dae5e7ab058a2a7175151574ad331a
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-03-30 15:22:26 +0900

    123

--- project2/project2.py
+++ project2/project2.py
@@ -15,9 +15,9 @@ heart_disease = True if hd == '심장병 있음' else False
 bp = st.sidebar.slider('안정혈압', 90, 200)
 col = st.sidebar.slider('콜레스테롤', 120, 564)
 hb = st.sidebar.slider('최대심박수', 70, 202, None)
-btn_clicked = st.sidebar.button("Confirm")
+btn_clicked1 = st.sidebar.button("Confirm")
 
-if btn_clicked == True:
+if btn_clicked1 == True:
     jobs = load("project2/xgb_model.joblib")
     tf = jobs.predict([[age,gender,heart_disease,bp,col,hb]])
     tf_p = jobs.predict_proba([[age,gender,heart_disease,bp,col,hb]])[:, 1]
@@ -25,48 +25,46 @@ if btn_clicked == True:
     st.title("🦾입력한 정보로 분석한 결과 입니다🦾")
     st.markdown("---")
 
-    col1, col2 = st.columns([4, 6])
-    with col1 :
-        if tf == 1 : st.write("# 분석 결과 🤦‍♂️ <span style='color:red'>고혈압</span> 🤦‍♂️입니다.", unsafe_allow_html=True)
-        elif tf == 0 : st.write("# 분석 결과 😊 <span style='color:blue'>정상</span> 😊입니다.", unsafe_allow_html=True)
-        else : pass
-        
-        st.write(f"""
-            ## 👇분석 결과👇
-            ### 👉 성별 : {sex}
-            ### 👉 나이 : {age}세
-            ### 👉 심장병(有, 無) : {hd}
-            ### 👉 혈압 : {bp}mmHg
-            ### 👉 콜레스트롤 : {col}TC
-            ### 👉 심박수 : {hb}bpm
-        """)
-    
-    with col2 : 
-        probabilities = []
-        for col_val in range(col, 150, -1):
-            tf_p = jobs.predict_proba([[age,gender,heart_disease,bp,col_val,hb]])[:, 1]
-            probabilities.append(tf_p)
-    
-            if tf_p < 0.5:
-                break
 
-
-        fig, ax = plt.subplots()
-        ax.plot(range(col, col-len(probabilities), -1), probabilities)
-        ax.set_xlabel("Cholesterol")
-        ax.set_ylabel("Probability of Heart Disease")
-        ax.set_title("Probability of Heart Disease by Cholesterol Level")
-        st.pyplot(fig)
+    if tf == 1 : st.write("# 분석 결과 🤦‍♂️ <span style='color:red'>고혈압</span> 🤦‍♂️입니다.", unsafe_allow_html=True)
+    elif tf == 0 : st.write("# 분석 결과 😊 <span style='color:blue'>정상</span> 😊입니다.", unsafe_allow_html=True)
+    else : pass
         
     st.write(f"""
-        ## 결과에 실망하지 마세요😭
-        ### 👇확률을 알려드립니다👇
-        ### 👉 {', '.join([f'{p*100:.4f}%' for p in tf_p])}
+        ## 👇분석 결과👇
+        ### 👉 성별 : {sex}
+        ### 👉 나이 : {age}세
+        ### 👉 심장병(有, 無) : {hd}
+        ### 👉 혈압 : {bp}mmHg
+        ### 👉 콜레스트롤 : {col}TC
+        ### 👉 심박수 : {hb}bpm
+    """)
+    st.write(f"""
+    ## 결과에 실망하지 마세요😭
+    ### 👇확률을 알려드립니다👇
+    ### 👉 {', '.join([f'{p*100:.4f}%' for p in tf_p])}
     """)    
-
-
-
-
+    break
 
 else:
     st.markdown('<div><a href="https://sparkly-prince-933.notion.site/1ccb865a95e54590bfd61e22b45520fa"><img src="https://i.imgur.com/ktulthH.gif" width=700></a></div>', unsafe_allow_html=True)
+
+btn_clicked2 = st.sidebar.button("Next")
+if btn_clicked2 == True:
+    probabilities = []
+    for col_val in range(col, 150, -1):
+        tf_p = jobs.predict_proba([[age,gender,heart_disease,bp,col_val,hb]])[:, 1]
+        probabilities.append(tf_p)
+
+        if tf_p < 0.5:
+            break
+
+
+    fig, ax = plt.subplots()
+    ax.plot(range(col, col-len(probabilities), -1), probabilities)
+    ax.set_xlabel("Cholesterol")
+    ax.set_ylabel("Probability of Heart Disease")
+    ax.set_title("Probability of Heart Disease by Cholesterol Level")
+    st.pyplot(fig)
+    
+
