e41ffd1879c3b4bc38d0892b25cad4614861d50c
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-04-03 11:22:35 +0900

    asd

--- project2/project2.py
+++ project2/project2.py
@@ -39,22 +39,22 @@ if btn_clicked1 == True:
     ### 👉 {', '.join([f'{p*100:.4f}%' for p in tf_p])}
     """)
     btn_clicked2 = st.button("Next")
+    while btn_clicked2 == True:
+        if tf == 1:
+            probabilities = []
+            for col_val in range(col, 150, -1):
+                prob = jobs.predict_proba([[age,gender,heart_disease,bp,col_val,hb]])[:, 1]
+                probabilities.append(prob)
+                if prob < 0.5:
+                    break
+            fig, ax = plt.subplots()
+            ax.plot(range(col, col-len(probabilities), -1), probabilities)
+            ax.set_xlabel("Cholesterol")
+            ax.set_ylabel("Probability of Heart Disease")
+            ax.set_title("Probability of Heart Disease by Cholesterol Level")
+            st.pyplot(fig)
+            gear.empty()
 
-    if btn_clicked2 == True and tf == 1:
-        probabilities = []
-        for col_val in range(col, 150, -1):
-            prob = jobs.predict_proba([[age,gender,heart_disease,bp,col_val,hb]])[:, 1]
-            probabilities.append(prob)
-            if prob < 0.5:
-                break
-        fig, ax = plt.subplots()
-        ax.plot(range(col, col-len(probabilities), -1), probabilities)
-        ax.set_xlabel("Cholesterol")
-        ax.set_ylabel("Probability of Heart Disease")
-        ax.set_title("Probability of Heart Disease by Cholesterol Level")
-        st.pyplot(fig)
-        gear.empty()
-
-if btn_clicked2 == True and tf == 0:
-    gear.empty()
-    st.image("https://i.imgur.com/4Xd3Mdn.gif", width = 800)
+        if tf == 0:
+            gear.empty()
+            st.image("https://i.imgur.com/4Xd3Mdn.gif", width = 800)
