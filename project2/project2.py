2731619ccbe7427930071488ae303f7bdf81fb17
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-04-03 10:08:38 +0900

    a

--- project2/project2.py
+++ project2/project2.py
@@ -54,8 +54,6 @@ if btn_clicked2 == True and tf == 1:
     ax.set_title("Probability of Heart Disease by Cholesterol Level")
     st.pyplot(fig)
     gear.empty()
-
+tf=0
 if btn_clicked2 == True and tf == 0:
     gear.image("https://i.imgur.com/4Xd3Mdn.gif", width = 800)
-
-btn_clicked2 = ""
