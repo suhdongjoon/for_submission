938822a265a328330f154781d008cc2ee2d563b9
suhdongjoon <101693995+suhdongjoon@users.noreply.github.com>
2023-04-03 13:58:30 +0900

    123

--- project2/project2_2.py
+++ project2/project2_2.py
@@ -38,9 +38,7 @@ if btn_clicked1 == True:
     ### 👇확률을 알려드립니다👇
     ### 👉 {', '.join([f'{p*100:.4f}%' for p in tf_p])}
     """)
-    btn_clicked2 = st.button("Next")
-while btn_clicked2 == True:
-    break
+btn_clicked2 = st.button("Next")
 if btn_clicked2 == True and tf == 1:
     probabilities = []
     for col_val in range(col, 150, -1):
@@ -55,6 +53,10 @@ if btn_clicked2 == True and tf == 1:
     ax.set_title("Probability of Heart Disease by Cholesterol Level")
     st.pyplot(fig)
     gear.empty()
+    btn_clicked2 = False
+while btn_clicked2 == True:
+    break
+st.image("https://i.imgur.com/4Xd3Mdn.gif", width = 800)
 
 if btn_clicked2 == True and tf == 0:
     gear.empty()
